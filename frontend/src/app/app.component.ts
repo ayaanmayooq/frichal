8ceddb121d988feb05146b493144dba38d8f43f2
shaikaya <shaikaya@msu.edu>
2023-05-24 16:50:12 -0400

    fontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AuthComponent } from './components/auth/auth.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frichal';
+}
